b777701e29540c4eb3c2cb356179281b8c156f6d
rgfathrg <rgfath@gmail.com>
2018-07-11 18:30:39 -0400

    random generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,12 @@ var wins;
 var losses;
 
 var futbolWords = ["goal", "pitch", "cleats", "supporters", "keeper", "striker", "ball", "midfielder"];
+var guessAttempts = [];
+
+function compWord() {
+    var compGuess = futbolWords[Math.floor(Math.random() * futbolWords.length)];
+    return compGuess;
+}
+
+var holdWord = compWord();
 
